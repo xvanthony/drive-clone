1abbc4b075e25b24582533fa9e54d81ccaddb238
xvanthony <anthonyroman729@gmail.com>
2025-12-01 16:49:52 -0800

    Database added

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,6 @@
 import { createClient, type Client } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
+import { drizzle } from "drizzle-orm/singlestore";
+import { createPool, type Pool } from "mysql2/promise";
 
 import { env } from "~/env";
 import * as schema from "./schema";
@@ -9,11 +10,28 @@ import * as schema from "./schema";
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
+  conn: Pool | undefined;
 };
 
-export const client =
-  globalForDb.client ?? createClient({ url: env.DATABASE_URL });
+const conn = 
+  globalForDb.conn ??
+  createPool({
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
+    maxIdle: 0,
+  });
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+conn.addListener('error', (err) => {
+  console.error('Database connection error:', err);
+});
+
+/*export const client =
+  globalForDb.conn ?? createClient({ url: env.DATABASE_URL });
 if (env.NODE_ENV !== "production") globalForDb.client = client;
 
-export const db = drizzle(client, { schema });
+export const db = drizzle(client, { schema }); */
